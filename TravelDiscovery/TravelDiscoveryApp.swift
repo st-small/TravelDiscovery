7bce22b0d0f460507dc02a82cdb9c7033774cd9f
Stanislav Shiyanovskiy <shiyanovskiy.s@richpeach.pro>
2021-03-10 19:01:40 +0200

    Initial Commit

---
+++ TravelDiscovery/TravelDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiscoveryApp.swift
+//  TravelDiscovery
+//
+//  Created by Stanly Shiyanovskiy on 10.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
